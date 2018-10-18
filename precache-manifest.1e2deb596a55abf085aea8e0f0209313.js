199148fe4e4c8c359405021a57da6fd749418d18
shootersf <me@simonfarrelly.com>
2018-10-18 22:16:22 +0100

    Updates

---
+++ precache-manifest.1e2deb596a55abf085aea8e0f0209313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ebd4b3436b6ded292ab",
+    "url": "/drink-picker/static/js/runtime~main.6ebd4b34.js"
+  },
+  {
+    "revision": "dbfccb1e8df488b9035b",
+    "url": "/drink-picker/static/js/main.dbfccb1e.chunk.js"
+  },
+  {
+    "revision": "d2a72ad7e0b312479945",
+    "url": "/drink-picker/static/js/1.d2a72ad7.chunk.js"
+  },
+  {
+    "revision": "dbfccb1e8df488b9035b",
+    "url": "/drink-picker/static/css/main.92dfc2fb.chunk.css"
+  },
+  {
+    "revision": "8464a484a318909d8b83ce07f1ca7455",
+    "url": "/drink-picker/index.html"
+  }
+];
